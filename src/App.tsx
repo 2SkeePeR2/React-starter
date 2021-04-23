be7bfcab9a205730962a810b3804bd107b0121b7
2SkeePeR2 <benten.ben14@gmail.com>
2021-04-23 17:26:55 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import "./App.css";
 import { store } from "./store/store";
 
 function App() {
-    return (
-        <Provider store={store}>
-            <React.Fragment>
-                <p>Hello world</p>
-            </React.Fragment>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <React.Fragment>
+        <p>Hello world</p>
+      </React.Fragment>
+    </Provider>
+  );
 }
 
 export default App;
